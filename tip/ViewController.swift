87e111d0e67fdabd2abf1a04352a923fb8ea910d
EricYang <ericyang@ericyangdemacbook-pro.local>
2016-07-04 17:22:19 +0800

    tip counter

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -21,5 +21,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var percentage: UILabel!
+    @IBOutlet weak var tip: UILabel!
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        percentage.text = String(sender.value)
+        if let a = Double(amount.text!){
+           tip.text = String(a*sender.value)
+        }
+    }
+    @IBAction func changeAmount(_ sender: UITextField) {
+        let per = percentage.text
+        let amo = sender.text
+        if let p = Double(per!), a = Double(amo!){
+            tip.text = String(p*a)
+        }
+        amount.text = amo
+    }
+    
 }
 
